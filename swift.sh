faf72b1a872e4421eb2a1ef5742b89cb4dba4c49
David Solberg <dwsolberg@gmail.com>
2016-01-11 14:27:34 -0600

    Fix to mustache file -- removing extraneous array.

--- swift.sh
+++ swift.sh
@@ -1,5 +1,5 @@
 java -jar ./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-  -i /Users/davidsolberg/Desktop/swagger.yaml \
+  -i /Users/davidsolberg/Dropbox/iBucket_swagger/api/swagger/swagger.yaml \
   -l swift \
   -t ./modules/swagger-codegen/src/main/resources/swift \
   -o /Users/davidsolberg/Desktop/generated/

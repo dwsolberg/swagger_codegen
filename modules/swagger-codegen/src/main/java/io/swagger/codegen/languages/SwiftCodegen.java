269d150b7dc83610e2c9fb49b2071e47a50dd69d
David Solberg <dwsolberg@gmail.com>
2016-04-12 18:58:32 -0500

    Adding core data builders for objects.

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SwiftCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SwiftCodegen.java
@@ -153,7 +153,9 @@ public class SwiftCodegen extends DefaultCodegen implements CodegenConfig {
     supportingFiles.add(new SupportingFile("Models.mustache", sourceFolder, "Models.swift"));
     supportingFiles.add(new SupportingFile("NetworkHelper.mustache", sourceFolder, "NetworkHelper.swift"));
     supportingFiles.add(new SupportingFile("OAuthAPI.mustache", sourceFolder, "OAuthAPI.swift"));
-      supportingFiles.add(new SupportingFile("xcdatamodel.mustache", sourceFolder, "CoreData.xcdatamodel"));
+    supportingFiles.add(new SupportingFile("xcdatamodel.mustache", sourceFolder, "CoreData.xcdatamodel"));
+    supportingFiles.add(new SupportingFile("CoreDataBuilders.mustache", sourceFolder, "CoreDataBuilders.swift"));
+    supportingFiles.add(new SupportingFile("CoreDataChangeCheckers.mustache", sourceFolder, "CoreDataChangeCheckers.swift"));
   }
 
   @Override
